9b9d8bddf85e6d784c2630356adef799a261fa56
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-11-05 16:12:53 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Ho Van Hanh hee sdfs h</h1>
+      <h1>Vite + React Ho Van Hanh hee sdfs h sdfs</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
